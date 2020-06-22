489ffd44c71adc93bfa4d87e7618474c0e2aa025
emin ucar <eminucar@hotmail.com>
2020-06-22 17:06:26 +0300

    Validate

---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        public static void CreatePasswordHash(string password,out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(GetByte)
+            }
+        }
+    }
+}
